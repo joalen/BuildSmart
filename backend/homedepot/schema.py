255cac08114a9eed6ea082e3dd6920b9da977ccd
Alen Jo <jo.alen1@outlook.com>
2026-04-16 01:20:32 -0500

    External linking of HD API + Dockerization
    - ok...so as it turns out, fetching the actual Home Depot API would be hard and it requires a bit of ingenuity in seeing how it unfolds
     - therefore...doing some network analysis and some initial prototyping...i figured out that invoking this API requires one to be in a browsing instance and then do a simple redirect to yield results to browser and fetch results
    - so initially I wanted to leave it as is...but then I figured if one wanted to host this on the cloud...then you might need to resort to dockerizing this
      - in my physical diagram...I kinda hinted at having this solution to happen through K8s happening
        - after a bit of development effort, I finally got the scaffold to work and was able to dockerize the backend
          - this is also hooked up to the npm command and should be solid for anyone to now run `npm run dev`

---
+++ backend/homedepot/schema.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from typing import List, Optional
+
+class Product(BaseModel):
+    itemId: str
+    brand: Optional[str] = None
+    name: Optional[str] = None
+    price: Optional[float] = None
+    image: Optional[str] = None
+    url: Optional[str] = None
+
+class SearchRequest(BaseModel):
+    keyword: str
+    storeId: Optional[str] = "6537"
+    zipCode: Optional[str] = "75150"
+    pageSize: Optional[int] = 12
+
+class SearchResponse(BaseModel):
+    keyword: str
+    products: List[Product]
+    total: Optional[int] = None
