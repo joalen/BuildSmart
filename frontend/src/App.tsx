e474210ca432dce38356ce75be20a99eb334baa0
Alen Jo <jo.alen1@outlook.com>
2026-04-16 01:53:19 -0500

    Attach Cost Estimate page + Receiving HD API
    - attached the HD API to the cost estimate page
    - attached that to the frontend for it to propagate and allow it to be displayed to user + sidebar as well

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,8 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Layout from '@/components/Layout'
 import DescribeProject from '@/pages/DescribeProject'
 import PlanResult from '@/pages/PlanResult'
+import CostEstimate from '@/pages/CostEstimate'
 //import Home from '@/pages/Home'
-//import Cost from '@/pages/Cost'
 //import Cart from '@/pages/Cart'
 //import Settings from '@/pages/Settings'
 
@@ -15,6 +15,7 @@ export default function App() {
           <Route index element={<Navigate to="/plan" replace />} />
           <Route path="plan" element={<DescribeProject />} />
           <Route path="plan/result" element={<PlanResult />} />
+          <Route path="cost" element={<CostEstimate />} />
         </Route>
       </Routes>
     </BrowserRouter>
