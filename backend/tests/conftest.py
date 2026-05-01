ba280245fc5b95c8ee0e18bd01cee30cb4721904
Alen Jo <jo.alen1@outlook.com>
2026-05-01 14:22:25 -0500

    Test Case #02 - Backend SKU validate + CI/CD
    - added in the CI/CD to make sure our tests run in an automated manner
    - also added in backend SKU validation to make test case assert what it should be

---
+++ backend/tests/conftest.py
@@ -0,0 +1,32 @@
+import sys, os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+
+import asyncio
+import pytest
+from homedepot.session import HomeDepotSession
+
+@pytest.fixture(scope="session")
+def hd_loop():
+    loop = asyncio.new_event_loop()
+    yield loop
+    loop.close()
+
+@pytest.fixture(scope="session")
+def hd_session(hd_loop):
+    session = HomeDepotSession()
+    hd_loop.run_until_complete(session.init())
+    hd_loop.run_until_complete(session.page.goto("https://www.homedepot.com"))
+    hd_loop.run_until_complete(session.page.wait_for_load_state("load"))
+    yield session
+    hd_loop.run_until_complete(session.close())
+
+@pytest.fixture
+def project_plan():
+    return {
+        "materials": [
+            {"name": "32 oz. All Purpose Cleaner Spray 30% Cleaning Vinegar", "sku": "313186760", "price": 8.71, "zip": "75150"},
+        ],
+        "tools": [
+            {"name": "9 in. Heavy Duty 5-Wire Paint Roller Frame", "sku": "100001737", "price": 4.68, "zip": "75150"},
+        ]
+    }
